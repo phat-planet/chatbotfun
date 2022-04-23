fdca8be8175c07ec7d5da6d7f0eb8d53addee288
Rafael Perez <PerezR6836@uhcl.edu>
2022-04-22 22:19:26 -0500

    speech to text enabled
    From here on out, builds will only work on a raspberry pi, that's what we're running

--- chatbot.py
+++ chatbot.py
@@ -4,9 +4,13 @@ import os
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 from gtts import gTTS
-from playsound import playsound
+# from playsound import playsound
+import pygame
+import speech_recognition as sr
+import time
 
 global mood
+
 
 # Upon running for the first time, you need to edit chatbotfun\venv\Lib\site-packages\sqlalchemy\util\compat.py
 # go to line 264, replace time.clock with time.process_time()
@@ -14,9 +18,25 @@ global mood
 def text2speech(tts):
     speech = gTTS(text=tts, lang='en', slow=False)
     speech.save("talk.mp3")
-    playsound("talk.mp3")
+    pygame.mixer.init()
+    nextline = pygame.mixer.music.load('talk.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    pygame.quit()
     os.remove("talk.mp3")
 
+
+# mic setup
+mic_name = "USB PnP Sound Device: Audio (hw:2,0)"
+sample_rate = 48000
+chunk_size = 2048
+r = sr.Recognizer()
+mic_list = sr.Microphone.list_microphone_names()
+for i, microphone_name in enumerate(mic_list):
+    print(microphone_name)
+    if microphone_name == mic_name:
+        device_id = i
 
 bad_words = ['jabroni', 'doofus', 'dumbass', 'pinhead']  # the bot will not say phrases containing these words
 
@@ -24,6 +44,7 @@ name = input('Please enter your name: ')  # we want the user's name
 
 bot = ChatBot(  # defining properties and attributes
     'spork',
+    read_only=True,
     storage_adapter='chatterbot.storage.SQLStorageAdapter',  # this defines the database the bot will use to learn
     preprocessors=[  # these will clean up the text so the bot can understand
         'chatterbot.preprocessors.clean_whitespace',
@@ -31,7 +52,9 @@ bot = ChatBot(  # defining properties and attributes
         'chatterbot.preprocessors.convert_to_ascii'
     ],
     logic_adapters=[
-
+        {
+            'import_path': 'camera_adapter.CameraAdapter'
+        },
         {
             'import_path': 'similar_response.SimilarResponseAdapter',
             'input_text': 'What is my name?',
@@ -52,9 +75,6 @@ bot = ChatBot(  # defining properties and attributes
         },
 
         'chatterbot.logic.MathematicalEvaluation',  # this gives the bot the ability to solve math equations
-        {
-            'import_path': 'camera_adapter.CameraAdapter'
-        },
         # {
         #   'import_path': 'mood_adapter.MoodAdapter'
         # },
@@ -66,6 +86,12 @@ bot = ChatBot(  # defining properties and attributes
     database_url='sqlite://database.sqlite3'
 )
 print('Starting bot. . . . ')
+
+# pygame.init()
+# mysfx = pygame.mixer.Sound('startup.wav')
+# mysfx.play()
+# pygame.time.wait(2000)
+# pygame.quit()
 
 while True:
     choice = input("Would you like to train the bot? Enter Y/N: ")  # train bot option
@@ -111,7 +137,9 @@ while True:
 
         trainer = ChatterBotCorpusTrainer(bot)
         trainer.train(
-            "chatterbot.corpus.custom"
+            "chatterbot.corpus.english.conversations",
+            "chatterbot.corpus.english.greetings",
+            "chatterbot.corpus.english.emotion"
         )
         ##################################
         #         END OF TRAINING        #
@@ -123,7 +151,11 @@ while True:
 while True:
     choice = input('Enable logging? Enter Y/N: ')  # logging option
     if choice == 'Y' or choice == 'y' or choice == 'yes':
-        logging.basicConfig(level=logging.INFO)  # enable logging
+        logging.basicConfig(filename="log.txt",
+                            level=logging.INFO,
+                            format='%(levelname)s: %(asctime)s %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S')
+        print('logging should be enabled')  # enable logging
         break
     if choice == 'N' or choice == 'n' or choice == 'no':
         break
@@ -146,13 +178,28 @@ while True:
         voice = False
         break
 
-if sfx:
-    print('Chatbot is now active')
+print('Chatbot is now active')
 
 while True:
     try:
+        starttimer = time.time()
+        with sr.Microphone(device_index=device_id, sample_rate=sample_rate,
+                           chunk_size=chunk_size) as source:
+            r.adjust_for_ambient_noise(source)
+            if sfx:
+                pygame.init()
+                mysfx = pygame.mixer.Sound('message.wav')
+                mysfx.play()
+                pygame.time.wait(300)
+                pygame.quit()
+            endtimer = time.time()
+            print(endtimer - starttimer)
+            print("Speak into the mic")
+            audio = r.listen(source)
+            request = r.recognize_google(audio)
+            print(name + ': ' + request)
 
-        request = (input(name + ': '))
+        # request = (input(name + ': '))
 
         # time.sleep(random.randint(1, 4))  # small delay, so the bot will act like it's "thinking"
         if request == "Bye" or request == 'bye' or request == 'goodbye' or request == 'Goodbye' or request == 'shut up':
@@ -167,5 +214,11 @@ while True:
             if voice:
                 tts = (str(response))
                 text2speech(tts)
+
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
+    except sr.UnknownValueError:
+        print('Chatbot: I couldn\'t understand you. Can you repeat that?')
+        if voice:
+            text2speech('I couldn\'t understand you. Can you repeat that?')
+        continue
